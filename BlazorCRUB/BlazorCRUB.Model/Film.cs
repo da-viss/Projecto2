2979422575d53037554edd1de89ee135350dc036
alumno.104416@ies-azarquiel.es <dragoneitor12>
2020-06-12 21:19:11 +0200

    Primer comit

---
+++ BlazorCRUB/BlazorCRUB.Model/Film.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorCRUB.Model
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
